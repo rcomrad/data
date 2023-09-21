209e32be5ccf88c665f847ff89c155fc164afe31
rcomrad <comrade_1997@mail.ru>
2023-09-21 18:18:38 +0300

    g

---
+++ front/assets/ModalButton-d354bf13.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-be40ab28.js";import"./bootstrap.bundle.min-ad4f528d.js";function e({connected_with:a,button_text:t,additionalClasses:o,preOpen:r}){return n.jsx("button",{type:"button",className:`btn btn-primary ${o}`,"data-bs-toggle":"modal",onClick:r,"data-bs-target":`#${a}`,children:t||"Click!"})}export{e as M};
--- front/assets/config-67b08cc3.js
+++
@@ -1,1 +0,0 @@
-const s="https://kusmirror.ru/api",r=20,t="https://kusmirror.ru";export{s as A,r as M,t as a};
