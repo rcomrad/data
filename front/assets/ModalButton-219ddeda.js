250eb76defcfa6e1f79d792b94da7020f33fabd1
Alex <comrade_1997@mail.ru>
2023-10-10 03:27:04 +0300

    g

--- front/assets/ModalButton-219ddeda.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-8cbc7bef.js";import"./bootstrap.bundle.min-ddb44f16.js";function e({connected_with:a,button_text:t,additionalClasses:o,preOpen:r}){return n.jsx("button",{type:"button",className:`btn btn-primary ${o}`,"data-bs-toggle":"modal",onClick:r,"data-bs-target":`#${a}`,children:t||"Click!"})}export{e as M};
