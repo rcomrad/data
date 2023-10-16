847426a9b0446388c1e8129ef6357bd3166136ad
Alex <comrade_1997@mail.ru>
2023-10-16 22:09:18 +0300

    g

---
+++ front/assets/Card-264ab620.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-832b76af.js";function c({children:s,additionalClasses:a}){return r.jsx("div",{className:`card ${a}`,children:r.jsx("div",{className:"card-body",children:s})})}export{c as C};
