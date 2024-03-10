33c109906aebf0359397916100e26314948d2d69
Alex <comrade_1997@mail.ru>
2024-03-10 23:47:02 +0300

    g

---
+++ front/assets/Homepage-wgOkpLKP.js
@@ -0,0 +1,7 @@
+import{r as e,_ as o,j as r,L as i}from"./index-oXycMzOM.js";import{G as a}from"./users-My4gugrs.js";function u(){const s=a(),t=e.lazy(()=>o(()=>import("./CompetitionList-TpSe5A-u.js"),__vite__mapDeps([0,1,2,3,4,5])));return r.jsx("div",{children:r.jsx(e.Suspense,{fallback:r.jsx(i,{}),children:s&&s.role.includes("submitor")?r.jsx(t,{}):null})})}export{u as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/CompetitionList-TpSe5A-u.js","assets/index-oXycMzOM.js","assets/index-kcKnW45L.css","assets/competition-RnXY66kS.js","assets/config-0xx7l4yL.js","assets/users-My4gugrs.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
