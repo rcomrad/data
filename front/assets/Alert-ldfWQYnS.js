08126af4290a3cff11357a6f475253ca4711b696
Alex <comrade_1997@mail.ru>
2024-03-10 23:37:21 +0300

    g

---
+++ front/assets/Alert-ldfWQYnS.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-Z1JudhZO.js";function l({type:r="primary",text:e,children:t}){return a.jsxs("div",{className:`alert alert-${r}`,role:"alert",children:[e,t]})}export{l as A};
---
+++ front/assets/RegistartionSuccess-ZOW55N5M.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Z1JudhZO.js";import{A as e}from"./Alert-ldfWQYnS.js";function i(){return t.jsx("div",{className:"full-height d-flex justify-content-center align-items-center",children:t.jsx(e,{text:"Мы послали письмо с инструкциями для завершения регистрации",type:"success"})})}export{i as default};
