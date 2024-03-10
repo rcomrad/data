4596d801bf82809e5770f418a1acfe5d2552f9d9
Alex <comrade_1997@mail.ru>
2024-03-11 00:24:31 +0300

    g

---
+++ front/assets/Homepage-05wnMAKO.js
@@ -0,0 +1,7 @@
+import{r as e,_ as o,j as r,L as i}from"./index-4N_kG8pY.js";import{G as a}from"./users-My4gugrs.js";function u(){const s=a(),t=e.lazy(()=>o(()=>import("./CompetitionList-7VcabADl.js"),__vite__mapDeps([0,1,2,3,4,5])));return r.jsx("div",{children:r.jsx(e.Suspense,{fallback:r.jsx(i,{}),children:s&&s.role.includes("submitor")?r.jsx(t,{}):null})})}export{u as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/CompetitionList-7VcabADl.js","assets/index-4N_kG8pY.js","assets/index-kcKnW45L.css","assets/competition-FRa9cu2U.js","assets/config-ZEIqkGks.js","assets/users-My4gugrs.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
