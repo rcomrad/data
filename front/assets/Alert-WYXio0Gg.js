de953a53eea581628da229fb1149988c95f0581a
Alex <comrade_1997@mail.ru>
2024-04-06 21:20:19 +0300

    f

---
+++ front/assets/Alert-WYXio0Gg.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-y6thpFE1.js";function l({type:r="primary",text:e,children:t}){return a.jsxs("div",{className:`alert alert-${r}`,role:"alert",children:[e,t]})}export{l as A};
---
+++ front/assets/Homepage-PLWGqRLC.js
@@ -0,0 +1,7 @@
+import{r as e,_ as o,j as r,L as i}from"./index-y6thpFE1.js";import{G as a}from"./users-My4gugrs.js";function u(){const s=a(),t=e.lazy(()=>o(()=>import("./CompetitionList-A-QqlGjj.js"),__vite__mapDeps([0,1,2,3,4,5])));return r.jsx("div",{children:r.jsx(e.Suspense,{fallback:r.jsx(i,{}),children:s&&s.role.includes("submitor")?r.jsx(t,{}):null})})}export{u as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/CompetitionList-A-QqlGjj.js","assets/index-y6thpFE1.js","assets/index-kcKnW45L.css","assets/competition-FRa9cu2U.js","assets/config-ZEIqkGks.js","assets/users-My4gugrs.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
